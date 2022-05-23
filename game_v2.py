9413bc6a03e617d14d52d154710827dc58e5de92
Malda04 <93832851+Malda04@users.noreply.github.com>
2022-05-23 22:28:00 +0300

    initial commit

---
+++ game_v2.py
@@ -0,0 +1,50 @@
+"""Game find number
+Компьютер сам загадывает и сам угадыват число"""
+
+from itertools import count
+import numpy as np
+
+def random_predict(number:int=1) ->int:
+    """Рандомно угадываем число
+
+    Args:
+        namber (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        : Число попыток
+    """
+    
+    count = 0
+    
+    while True:
+        count+=1
+        predict_number = np.random.randint(1, 101) # предпологаемое число попыток
+        if number == predict_number:
+            break # Выход из цыкла
+    return(count)
+
+def score_game(random_predict) ->int:
+    """За какое число попыток в среднем из 1000 подходов угадывает 
+    наш алгоритм
+
+    Args:
+        random_predict (_type_): Функция угадывания
+
+    Returns:
+        int: среднее кол-во попыток
+    """
+    
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    
+    for number in random_array:
+        count_ls.append(random_predict(number))
+        
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+    
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+
+if __name__ == '__main__':
+    score_game(random_predict)
