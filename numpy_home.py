c65c956cca3cc779dcebaa1fd2180800b9950210
Malda04 <93832851+Malda04@users.noreply.github.com>
2022-06-08 13:30:18 +0300

    edit numpy_home.py

--- numpy_home.py
+++ numpy_home.py
@@ -5,8 +5,11 @@ import numpy as np
 N_min = -(2**16)/2
 N_max = (2**16)/2-1
 
-#print(N_min, N_max)
+#print(N_min, N_max) 
 
-a = np.uint8(25)
-print(a)
-print(np.iinfo(a))
+a = np.int8(-456)
+#print(a)
+#print(np.iinfo(a)) # min = -128 max = 127
+
+arr = np.array([1,5,2,9,10])
+print(arr.dtype) # int32
